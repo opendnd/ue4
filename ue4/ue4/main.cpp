b16e27e4270e7aa2abb9e7dad4b0aab6ca02f647
Drew Morris <dhmorris@gmail.com>
2018-06-28 22:36:24 -0400

    Working on the CLI

---
+++ ue4/ue4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ue4
+//
+//  Created by Drewry Morris on 4/22/18.
+//  Copyright © 2018 OpenDnD. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
