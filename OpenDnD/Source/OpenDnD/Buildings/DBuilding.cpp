ee4e95c618d0badd43e6889d4164514700cb47f6
Drew Morris <dhmorris@gmail.com>
2019-01-12 12:04:01 -0500

    Working on more interaction

---
+++ OpenDnD/Source/OpenDnD/Buildings/DBuilding.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DBuilding.h"
+
+// Sets default values
+ADBuilding::ADBuilding()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADBuilding::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADBuilding::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ OpenDnD/Source/OpenDnD/Quests/DQuest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DQuest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENDND_API UDQuest : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
